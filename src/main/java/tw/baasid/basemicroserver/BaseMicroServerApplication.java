892c20e6d0468033e9fff3e35ea5e70a4ebf3524
a28115665 <a28115665@gmail.com>
2022-10-18 11:12:14 +0800

    first commit

---
+++ src/main/java/tw/baasid/basemicroserver/BaseMicroServerApplication.java
@@ -0,0 +1,13 @@
+package tw.baasid.basemicroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseMicroServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseMicroServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/baasid/basemicroserver/BaseMicroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.baasid.basemicroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseMicroServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
